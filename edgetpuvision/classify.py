ed16abafdab4d0efb23f1df4dd902120cb0c701a
Dmitry Kovalev <dkovalev@google.com>
2019-03-20 15:39:34 -0700

    Add license headers.
    Change-Id: I6fb2e7e94a164059e21de2078954ab34504d4e2f

--- edgetpuvision/classify.py
+++ edgetpuvision/classify.py
@@ -1,11 +1,25 @@
-"""A demo which runs object classification on camera frames."""
-
-# export TEST_DATA=/usr/lib/python3/dist-packages/edgetpu/test_data
+# Copyright 2019 Google LLC
 #
-# python3 -m edgetpuvision.classify \
-#   --model ${TEST_DATA}/mobilenet_v2_1.0_224_inat_bird_quant.tflite \
-#   --labels ${TEST_DATA}/inat_bird_labels.txt
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+"""A demo which runs object classification on camera frames.
+
+export TEST_DATA=/usr/lib/python3/dist-packages/edgetpu/test_data
+
+python3 -m edgetpuvision.classify \
+  --model ${TEST_DATA}/mobilenet_v2_1.0_224_inat_bird_quant.tflite \
+  --labels ${TEST_DATA}/inat_bird_labels.txt
+"""
 import argparse
 import collections
 import itertools
